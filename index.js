8650dfdd8edac2a3a1fe672dc194819b48470102
Isaiah <isaiahkantor23@gmail.com>
2024-05-27 21:21:51 -0400

    Done Today

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function countDown(number) {
+    while (number > 0) {
+      console.log(number); // Output the current number
+      number--; // Decrement the number by 1
+    }
+    console.log(number); // Output zero
+  }
+
